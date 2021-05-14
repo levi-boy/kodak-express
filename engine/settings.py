33fe9eef3c3b6507aaa4da9aee693f80e87f69e7
Levi Boy <levi.boy@yandex.ru>
2021-05-14 18:32:21 +0500

    make it better

--- engine/settings.py
+++ engine/settings.py
@@ -149,6 +149,6 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
